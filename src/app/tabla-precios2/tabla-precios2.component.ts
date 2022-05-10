e77fa9d35274db6fce4e8a110b1c4f398397a47a
Gustavo <gusgoludo9@gmail.com>
2022-05-10 13:47:59 -0500

    2.

---
+++ src/app/tabla-precios2/tabla-precios2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-precios2',
+  templateUrl: './tabla-precios2.component.html',
+  styleUrls: ['./tabla-precios2.component.scss']
+})
+export class TablaPrecios2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
