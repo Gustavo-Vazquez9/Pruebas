0facf7a117d388a55740d832d536411c84e51be3
Gustavo <gusgoludo9@gmail.com>
2022-05-10 13:45:53 -0500

    1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaPreciosComponent } from './tabla-precios/tabla-precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaPreciosComponent
   ],
   imports: [
     BrowserModule,
